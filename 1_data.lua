6e3b9405888db62800bef8b9e048d570f0415172
tethys <petrescu.viviana@gmail.com>
2015-04-07 14:42:30 +0200

    added initial .lua files

---
+++ 1_data.lua
@@ -0,0 +1,184 @@
+----------------------------------------------------------------------
+-- This script demonstrates how to load the (SVHN) House Numbers 
+-- training data, and pre-process it to facilitate learning.
+--
+-- The SVHN is a typicaly example of supervised training dataset. 
+-- The problem to solve is a 10-class classification problem, similar
+-- to the quite known MNIST challenge.
+--
+-- It's a good idea to run this script with the interactive mode:
+-- $ torch -i 1_data.lua
+-- this will give you a Torch interpreter at the end, that you
+-- can use to analyze/visualize the data you've just loaded.
+--
+-- Clement Farabet
+----------------------------------------------------------------------
+
+require 'torch'   -- torch
+require 'image'   -- for color transforms
+require 'nn'      -- provides a normalization operator
+
+----------------------------------------------------------------------
+-- parse command line arguments
+if not opt then
+   print '==> processing options'
+   cmd = torch.CmdLine()
+   cmd:text()
+   cmd:text('SVHN Dataset Preprocessing')
+   cmd:text()
+   cmd:text('Options:')
+   cmd:option('-size', 'small', 'how many samples do we load: small | full | extra')
+   cmd:option('-visualize', true, 'visualize input data and weights during training')
+   cmd:text()
+   opt = cmd:parse(arg or {})
+end
+
+----------------------------------------------------------------------
+print '==> downloading dataset'
+
+-- Here we download dataset files. 
+
+-- Note: files were converted from their original Matlab format
+-- to Torch's internal format using the mattorch package. The
+-- mattorch package allows 1-to-1 conversion between Torch and Matlab
+-- files.
+
+-- The SVHN dataset contains 3 files:
+--    + train: training data
+--    + test:  test data
+--    + extra: extra training data
+
+-- By default, we don't use the extra training data, as it is much 
+-- more time consuming
+
+
+----------------------------------------------------------------------
+-- training/test size
+opt.size = 'small'
+if opt.size == 'small' then
+   print '==> using reduced training data, for fast experiments'
+   trsize = 1000
+   tesize = 200
+end
+
+----------------------------------------------------------------------
+print '==> loading dataset'
+
+-- We load the dataset from disk, and re-arrange it to be compatible
+-- with Torch's representation. Matlab uses a column-major representation,
+-- Torch is row-major, so we just have to transpose the data.
+
+-- Note: the data, in X, is 4-d: the 1st dim indexes the samples, the 2nd
+-- dim indexes the color channels (RGB), and the last two dims index the
+-- height and width of the samples.
+noutputs = 1
+trainData = {
+   data = torch.randn(trsize, 1, 1,32),
+   labels = torch.randn(trsize, noutputs),
+   size = function() return trsize end
+}
+
+-- If extra data is used, we load the extra file, and then
+-- concatenate the two training sets.
+
+-- Torch's slicing syntax can be a little bit frightening. I've
+-- provided a little tutorial on this, in this same directory:
+-- A_slicing.lua
+
+-- Finally we load the test data.
+
+testData = {
+   data = torch.randn(tesize, 1, 1,32),
+   labels = torch.randn(tesize, noutputs),
+   size = function() return tesize end
+}
+
+----------------------------------------------------------------------
+print '==> preprocessing data'
+
+-- Preprocessing requires a floating point representation (the original
+-- data is stored on bytes). Types can be easily converted in Torch, 
+-- in general by doing: dst = src:type('torch.TypeTensor'), 
+-- where Type=='Float','Double','Byte','Int',... Shortcuts are provided
+-- for simplicity (float(),double(),cuda(),...):
+
+trainData.data = trainData.data:float()
+testData.data = testData.data:float()
+
+-- We now preprocess the data. Preprocessing is crucial
+-- when applying pretty much any kind of machine learning algorithm.
+
+-- For natural images, we use several intuitive tricks:
+--   + images are mapped into YUV space, to separate luminance information
+--     from color information
+--   + the luminance channel (Y) is locally normalized, using a contrastive
+--     normalization operator: for each neighborhood, defined by a Gaussian
+--     kernel, the mean is suppressed, and the standard deviation is normalized
+--     to one.
+--   + color channels are normalized globally, across the entire dataset;
+--     as a result, each color component has 0-mean and 1-norm across the dataset.
+
+
+-- Name channels for convenience
+channels = {'y'}
+
+-- Normalize each channel, and store mean/std
+-- per channel. These values are important, as they are part of
+-- the trainable parameters. At test time, test data will be normalized
+-- using these values.
+print '==> preprocessing data: normalize each feature (channel) globally'
+mean = {}
+std = {}
+for i,channel in ipairs(channels) do
+   -- normalize each channel globally:
+   mean[i] = trainData.data[{ {},i,{},{} }]:mean()
+   std[i] = trainData.data[{ {},i,{},{} }]:std()
+   trainData.data[{ {},i,{},{} }]:add(-mean[i])
+   trainData.data[{ {},i,{},{} }]:div(std[i])
+end
+
+-- Normalize test data, using the training means/stds
+for i,channel in ipairs(channels) do
+   -- normalize each channel globally:
+   testData.data[{ {},i,{},{} }]:add(-mean[i])
+   testData.data[{ {},i,{},{} }]:div(std[i])
+end
+
+
+----------------------------------------------------------------------
+print '==> verify statistics'
+
+-- It's always good practice to verify that data is properly
+-- normalized.
+
+for i,channel in ipairs(channels) do
+   trainMean = trainData.data[{ {},i }]:mean()
+   trainStd = trainData.data[{ {},i }]:std()
+
+   testMean = testData.data[{ {},i }]:mean()
+   testStd = testData.data[{ {},i }]:std()
+
+   print('training data, '..channel..'-channel, mean: ' .. trainMean)
+   print('training data, '..channel..'-channel, standard deviation: ' .. trainStd)
+
+   print('test data, '..channel..'-channel, mean: ' .. testMean)
+   print('test data, '..channel..'-channel, standard deviation: ' .. testStd)
+end
+
+----------------------------------------------------------------------
+print '==> visualizing data'
+
+-- Visualization is quite easy, using itorch.image().
+
+if opt.visualize then
+   if itorch then
+   first256Samples_y = trainData.data[{ {1,256},1 }]
+   first256Samples_u = trainData.data[{ {1,256},2 }]
+   first256Samples_v = trainData.data[{ {1,256},3 }]
+   itorch.image(first256Samples_y)
+   itorch.image(first256Samples_u)
+   itorch.image(first256Samples_v)
+   else
+      print("For visualization, run this script in an itorch notebook")
+   end
+end
---
+++ 3_loss.lua
@@ -0,0 +1,63 @@
+----------------------------------------------------------------------
+-- This script demonstrates how to define a couple of different
+-- loss functions:
+--   + negative-log likelihood, using log-normalized output units (SoftMax)
+--   + mean-square error
+--   + margin loss (SVM-like)
+--
+-- Clement Farabet
+----------------------------------------------------------------------
+
+require 'torch'   -- torch
+require 'nn'      -- provides all sorts of loss functions
+
+----------------------------------------------------------------------
+-- parse command line arguments
+if not opt then
+   print '==> processing options'
+   cmd = torch.CmdLine()
+   cmd:text()
+   cmd:text('SVHN Loss Function')
+   cmd:text()
+   cmd:text('Options:')
+   cmd:option('-loss', 'nll', 'type of loss function to minimize: nll | mse | margin')
+   cmd:text()
+   opt = cmd:parse(arg or {})
+
+   -- to enable self-contained execution:
+   model = nn.Sequential()
+end
+
+-- 10-class problem
+noutputs = 1
+opt.loss = 'mse'
+----------------------------------------------------------------------
+print '==> define loss'
+
+
+if opt.loss == 'mse' then
+
+   -- for MSE, we add a tanh, to restrict the model's output
+  --   model:add(nn.Tanh())
+
+   -- The mean-square error is not recommended for classification
+   -- tasks, as it typically tries to do too much, by exactly modeling
+   -- the 1-of-N distribution. For the sake of showing more examples,
+   -- we still provide it here:
+
+   criterion = nn.MSECriterion()
+   criterion.sizeAverage = false
+
+   -- Compared to the other losses, the MSE criterion needs a distribution
+   -- as a target, instead of an index. Indeed, it is a regression loss!
+   -- So we need to transform the entire label vectors:
+
+else
+
+   error('unknown -loss')
+
+end
+
+----------------------------------------------------------------------
+print '==> here is the loss function:'
+print(criterion)
