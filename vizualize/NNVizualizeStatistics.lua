2d8ef4afa2cc60fb44d58c0fa18e64e1581866de
tethys <petrescu.viviana@gmail.com>
2015-06-05 14:45:12 +0200

    added 3D plot of the weights

--- vizualize/NNVizualizeStatistics.lua
+++ vizualize/NNVizualizeStatistics.lua
@@ -1,6 +1,9 @@
 require 'gnuplot'
 require 'string'
+require 'os'
 require 'nn'
+require 'sys'
+
 local NNVizualizeStatistics = torch.class('NNVizualizeStatistics')
 -- TODO consider adding this to Sequential
 --
@@ -17,19 +20,36 @@ function NNVizualizeStatistics:plotAll(model)
     gnuplot.raw('load parula.pal')
     nbr_modules = nbr_parameters/2
     command_string = string.format("set multiplot layout %d,2", nbr_modules)
-    gnuplot.raw(command_string)
+--    gnuplot.raw(command_string)
     gnuplot.raw('title "My special plot"')
     for i=1,nbr_modules do
-        gnuplot.raw(string.format('lt %d', 2*(i-1)+1))
-        gnuplot.imagesc(model:parameters()[2*i-1], 'color')
-        gnuplot.raw(string.format('lt %d', 2*i))
+--        gnuplot.raw(string.format('lt %d', 2*(i-1)+1))
+        -- save weights to file
+        self:plotWeights(model:parameters()[2*i-1] ,i)
+--        gnuplot.imagesc(model:parameters()[2*i-1], 'color')
+--        gnuplot.raw(string.format('lt %d', 2*i))
         double_bias = model:parameters()[2*i]:clone()
         double_bias = torch.repeatTensor(double_bias,2,2) 
-        gnuplot.imagesc(double_bias)
+ --       gnuplot.imagesc(double_bias)
     end
 end
 
-function NNVizualizeStatistics:plotWeights()
+function NNVizualizeStatistics:plotWeights(weights, nbr)
+    N1 = weights:size(1)
+    N2 = weights:size(2)
+    weightsfile = string.format('weights%d', nbr)
+    file = torch.DiskFile(weightsfile,'w')
+    for i=1,N1 do
+        for j=1,N2 do
+            line_entry = string.format("%d %d %f\n", i, j, weights[{i, j}]) 
+            file:writeString(line_entry)
+        end
+    end
+    file:close()
+    gnuplot.raw(string.format('set term wxt %d', nbr -1))
+    gnuplot.raw(string.format('set dgrid3d %d ,%d', N1, N2))
+    gnuplot.raw('set hidden3d')
+    gnuplot.raw(string.format('splot "%s" u 1:2:3 with lines', weightsfile))
 end
 
 function NNVizualizeStatistics:plotActivations()
